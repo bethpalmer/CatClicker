4d155f7f325c8140c9fa096189ccf19bb08d9f60
Beth Palmer <Beth@MacBook-Pro.local>
2017-05-14 21:30:05 +0100

    Everything displays correctly, but click / counter on cat image is not yet functioning

---
+++ README.md
@@ -0,0 +1,12 @@
+README.md
+
+1. Load List of Cats and also empty cat display area
+2. Make each <li> clickable
+3. [LIST CLICK HANDLER] When clicked, display cat details in display area
+
+// THIS IS WHERE I GOT STUCK - MAKING THE CLICKER REGISTER A CLICK ON THE IMAGE. 
+
+4. When cat details are displayed in display area - if img exists...? - make it clickable.
+5. Make each cat retain its number of clicks...
+
+
--- js/main.js
+++ js/main.js
@@ -29,46 +29,102 @@ var cats = [{
 	}
 ];
 
-function loadCats(array){
+// Creates empty cat display area to be used later
+function loadCatDisplay(){
+	var catDisplay = document.createElement('div');
+	catDisplay.id =('selected')
+	// var catName = document.createElement('h3');
+	// var image = document.createElement('img');
+	// var count = document.createElement('p');
+	// catDisplay.appendChild(catName, image, count);
+	document.body.appendChild(catDisplay);
+	// console.log("Empty div is created")
+}
 
+// Loads list of cat names
+function loadList(array){
+	var catList = document.createElement('ul');
 	for (var i=0; i<array.length; i++){
-		var catDisplay = document.createElement('div');
+		var catName = document.createElement('li');
+		catName.innerHTML = array[i].name;
+		catList.appendChild(catName);
+	};
+	document.body.appendChild(catList);
+	loadCatDisplay();
+};
 
-		var catName = document.createElement('h3');
-	    catName.innerHTML = array[i].name;
-	    catDisplay.appendChild(catName);
+// function clickPic(img){
 
-		var image = document.createElement('img');
-	    image.src = array[i].pic;
-	    image.className = ('cat');
-	    image.style.width = '200px';
-	    catDisplay.appendChild(image);
+// 	var img = document.getElementById('cat');
 
-	    var count = document.createElement('p');
-		count.innerHTML = array[i].click;
-		catDisplay.appendChild(count);
+// 	console.log(img);
+// }
 
-	    document.body.appendChild(catDisplay);
-	}	
+
+// Makes cat names clickable and loads cat details into catDisplay div
+function catSelect(array){
+	var elem = document.getElementsByTagName('li');
+	for (var i=0; i<elem.length; i++){
+		elem[i].addEventListener('click', clickHandler(i));
+	};
+	function clickHandler(i){
+		return function() {
+			var catDisplay = document.getElementById('selected');
+
+			// First clear the div, then write new info to it
+			catDisplay.innerHTML = "";
+
+			var catName = document.createElement('h3');
+		    catName.innerHTML = array[i].name;
+		    catDisplay.appendChild(catName);
+
+			var image = document.createElement('img');
+		    image.src = array[i].pic;
+		    image.id = ('cat');
+		    image.style.width = '200px';
+		    catDisplay.appendChild(image);
+
+		    var count = document.createElement('p');
+			count.innerHTML = array[i].click;
+			catDisplay.appendChild(count);
+		}
+	};
+
+
+	// function clickCats(array){
+	// 	var catPic = document.getElementsByClassName('cat');
+		
+	// 	for (var i=0; i<catPic.length; i++){
+	// 		catPic[i].addEventListener('click', clickHandler(i));
+	// 	};
+
+	// 	function clickHandler(i){
+	// 		return function(){
+	//             var noOfClicks = cats[i].click += 1;
+	// 			var counter = document.getElementsByTagName('p');
+	// 			counter[i].innerHTML = noOfClicks;
+	//         };
+	// 	};
+	// }(cats);
+
 }
 
 //////////// MAKE EACH IMG.CAT CLICKABLE AND RECORD CLICKS IN COUNTER //////////////
 
-function clickCats(array){
-	var elem = document.getElementsByClassName('cat');
 
-	for (var i=0; i<elem.length; i++){
-		elem[i].addEventListener('click', clickHandler(i));
-	};
+loadList(cats);
+catSelect(cats);
 
-	function clickHandler(i){
-		return function(){
-            var noOfClicks = cats[i].click += 1;
-			var counter = document.getElementsByTagName('p');
-			counter[i].innerHTML = noOfClicks;
-        };
-	};
-};
 
-loadCats(cats);
-clickCats(cats);
+
+
+
+
+
+
+/* 	1. Display List of cat names
+		- create <ul>
+		- write each cat[i].name to each <li>
+	2. Let user select one
+		- onClick create cat div with name / pic / counter
+		- if user selects another then cat div needs to be overwirtten */
